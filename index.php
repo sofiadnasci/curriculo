5a058fdf207cc70c226cc5682fc5c1dd72f2b788
sofia du <128975035+sofiadnasci@users.noreply.github.com>
2023-05-07 21:35:57 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script scr="main.js"></script>
+    <title>Portfólio | Sofia Duarte</title>
+</head>
+<header>
+   <!-- Navbar com minha logo -->
+   <img class="logo" src="img/logo.png" height="100" >  
+        <nav>
+            <ul class="nav__links">
+                <li>
+                    <p href="#">Sobre mim</p>
+                </li>
+                <li>
+                    <p href="#">Projetos</p>
+                </li>
+                <li>
+                    <p href="#">Habilidades</p>
+                </li>
+            </ul>
+        </nav>
+        <p class="but" href="#">
+        <button type="button" class="git">GitHub</button>
+        </p>
+</header>
+<body>
+    <section class="sec1">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 mt-5 pt-5">
+                    <div class="align-middle">
+                        <h1 class="display-2">Olá, mundo!</h1>
+                        <h3 class="display-7">sejam bem-vindos ao meu portfólio ;)</h>
+                    </div>
+                </div>
+                    <div class="col-md-auto">
+                        <img class="logo" src="img/monstri.svg" height="600">
+                    </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
